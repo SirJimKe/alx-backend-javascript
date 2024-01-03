998132f7a29067fcde50291adddaa03ed79f6de5
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-01-03 19:12:01 +0300

    user promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise
+    .all([uploadPhoto(), createUser()])
     .then(([photoResult, userResult]) => {
       console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
     })
-    .catch(() => console.error('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,8 @@
+export default function signUpUser(firstName, lastName) {
+  const userObject = {
+    firstName,
+    lastName,
+  };
+
+  return Promise.resolve(userObject);
+}
