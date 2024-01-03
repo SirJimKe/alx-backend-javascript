34482da04839c0c3f4283ad3969dd5f3e4d460ed
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-01-03 05:56:36 +0300

    iterators

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,11 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const employeeNames = [];
+
+  for (const employee of reportWithIterator) {
+    employeeNames.push(employee);
+  }
+
+  const resultString = employeeNames.join(' | ');
+
+  return resultString;
+}
