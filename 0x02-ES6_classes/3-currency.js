2b9537e50889d480bdf6a9d008f252765fafa63c
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-01-05 04:55:51 +0300

    pricing

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,33 +1,26 @@
 export default class Currency {
   constructor(code, name) {
-    this._code = this.validateString(code, 'code');
-    this._name = this.validateString(name, 'name');
+    this._code = code;
+    this._name = name;
   }
 
   get code() {
     return this._code;
   }
 
-  set code(value) {
-    this._code = this.validateString(value, 'code');
+  set code(newCode) {
+    this._code = newCode;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    this._name = this.validateString(value, 'name');
+  set name(newName) {
+    this._name = newName;
   }
 
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
-  }
-
-  validateString(value, attributeName) {
-    if (typeof value !== 'string') {
-      throw new Error(`${attributeName} must be a string`);
-    }
-    return value;
   }
 }
