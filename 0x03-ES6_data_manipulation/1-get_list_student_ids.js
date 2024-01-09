2e68887b735a4950ce9466e78db601cdf8c8a97c
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-01-09 23:50:35 +0300

    reduce

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,6 @@
 export default function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
-    return [];
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
   }
-
-  const ids = students.map((student) => student.id);
-  return ids;
+  return [];
 }
---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,5 @@
+export default function getStudentIdsSum(students) {
+  const sumOfIds = students.reduce((accumulator, student) => accumulator + student.id, 0);
+
+  return sumOfIds;
+}
