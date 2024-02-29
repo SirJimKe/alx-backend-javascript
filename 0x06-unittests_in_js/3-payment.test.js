c56f50b380b51c82a14abd095879f54a7e00caf4
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-02-29 22:40:10 +0300

    using spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sentPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+    it('should call Utils.calculateNumber with correct arguments', () => {
+	const calcNumSpy = sinon.spy(Utils, 'calculateNumber');
+
+	sendPaymentRequestToApi(100, 20);
+
+	expect(calcNumSpy.calledOnce).to.be.true;
+	expect(calcNumSpy.calledWithExactly('SUM', 100, 20)).to.be.true;
+
+	calculateNumberSpy.restore();
+    });
+});
